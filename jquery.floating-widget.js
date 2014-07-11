1bff2c14be537b48878ea90957538f9d91633b5d
kawasaki kohei <kawasaki@ficc.jp>
2014-07-11 18:20:46 +0900

    断じてスペース2個

--- jquery.floating-widget.js
+++ jquery.floating-widget.js
@@ -6,32 +6,32 @@
  * Licensed under the MIT license: http://www.opensource.org/licenses/MIT
  */
 (function($) {
-    $.fn.floatingWidget = function(options) {
-        var opts = $.extend({
-            paddingTop: 0
-        }, options);
-        return this.each(function() {
-            var $this = $(this),
-                $parent = $this.offsetParent(),
-                $window = $(window),
-                top = $this.offset().top - parseFloat($this.css('marginTop').replace(/auto/, 0)),
-                floatingClass = 'floating';
-            if ($parent.height() > $this.outerHeight(true)) {
-                $window.on('scroll.floating-widget', function() {
-                    var y = $window.scrollTop() + opts.paddingTop,
-                        bottom = $parent.offset().top + $parent.height() - $this.outerHeight(true);
-                    if (y > top) {
-                        $this.addClass(floatingClass);
-                    } else {
-                        $this.removeClass(floatingClass);
-                    }
-                });
-            }
+  $.fn.floatingWidget = function(options) {
+    var opts = $.extend({
+      paddingTop: 0
+    }, options);
+    return this.each(function() {
+      var $this = $(this),
+        $parent = $this.offsetParent(),
+        $window = $(window),
+        top = $this.offset().top - parseFloat($this.css('marginTop').replace(/auto/, 0)),
+        floatingClass = 'floating';
+      if ($parent.height() > $this.outerHeight(true)) {
+        $window.on('scroll.floating-widget', function() {
+          var y = $window.scrollTop() + opts.paddingTop,
+            bottom = $parent.offset().top + $parent.height() - $this.outerHeight(true);
+          if (y > top) {
+            $this.addClass(floatingClass);
+          } else {
+            $this.removeClass(floatingClass);
+          }
         });
-    };
-    $.fn.floatingWidgetDestroy = function() {
-        var $window = $(window);
-        $window.off('scroll.floating-widget');
-        $(this).removeClass('floating');
-    };
+      }
+    });
+  };
+  $.fn.floatingWidgetDestroy = function() {
+    var $window = $(window);
+    $window.off('scroll.floating-widget');
+    $(this).removeClass('floating');
+  };
 })(jQuery);
